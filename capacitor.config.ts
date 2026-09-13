d917a9e49f95684e82519d1260a39f7f355fae41
Kumaresh <kumaresh.anandan@royalsundaram.in>
2026-09-13 11:12:47 +0530

    capacitor change

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.carpool.app',
   appName: 'CarShare247',
-  webDir: 'dist/car-pool'
+  webDir: 'dist/car-pool/browser'
 };
 
 export default config;
